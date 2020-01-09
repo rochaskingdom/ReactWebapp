91a3581a567c8a4dc46c813944d63279cbf553d7
Vinicius Rocha <viniciusr.rocha@outlook.com>
2020-01-09 10:48:07 -0300

    Reutilização de componentes e parametrização

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
 import $ from 'jquery';
+import InputCustomizado from './components/InputCustomizado';
 
 class App extends Component {
 
   constructor() {
-    super(); 
+    super();    
     this.state = {lista : [],nome:'',email:'',senha:''};
     this.enviaForm = this.enviaForm.bind(this);
     this.setNome = this.setNome.bind(this);
@@ -14,52 +15,51 @@ class App extends Component {
     this.setSenha = this.setSenha.bind(this);
   }
 
-  componentDidMount() {
+  componentDidMount(){  
     $.ajax({
-      url:"http://localhost:8080/api/autores",
-      dataType: 'json',
-      success:function(resposta) {
-        this.setState({lista:resposta});
-      }.bind(this)
-    });
+        url:"http://localhost:8080/api/autores",
+        dataType: 'json',
+        success:function(resposta){    
+          this.setState({lista:resposta});
+        }.bind(this)
+      } 
+    );          
   }
 
-  enviaForm(evento) {
-    evento.preventDefault();
+  enviaForm(evento){
+    evento.preventDefault();    
     $.ajax({
       url:'http://localhost:8080/api/autores',
       contentType:'application/json',
       dataType:'json',
       type:'post',
       data: JSON.stringify({nome:this.state.nome,email:this.state.email,senha:this.state.senha}),
-      success: function(resposta) {
-        console.log('enviado com sucesso');
-      },
-      error: function(resposta) {
-        console.log('erro');
-      }
+      success: function(resposta){
+        this.setState({lista:resposta});        
+      }.bind(this),
+      error: function(resposta){
+        console.log("erro");
+      }      
     });
   }
 
-  setNome(evento) {
+  setNome(evento){
     this.setState({nome:evento.target.value});
   }
 
-  setEmail(evento) {
+  setEmail(evento){
     this.setState({email:evento.target.value});
-  }
+  }  
 
-  setSenha (evento) {
+  setSenha(evento){
     this.setState({senha:evento.target.value});
   }
-
-
-  render() {
+  render() {    
     return (
       <div id="layout">
-
+          
           <a href="#menu" id="menuLink" className="menu-link">
-
+              
               <span></span>
           </a>
 
@@ -72,7 +72,7 @@ class App extends Component {
                       <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
                       <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
 
-
+                      
                   </ul>
               </div>
           </div>
@@ -83,19 +83,10 @@ class App extends Component {
                   </div>
                   <div className="content" id="content">
                     <div className="pure-form pure-form-aligned">
-                      <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm.bind} method="post">
-                        <div className="pure-control-group">
-                          <label htmlFor="nome">Nome</label> 
-                          <input id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome}/>                  
-                        </div>
-                        <div className="pure-control-group">
-                          <label htmlFor="email">Email</label> 
-                          <input id="email" type="email" name="email"  value={this.state.email} onChange={this.setEmail}/>                  
-                        </div>
-                        <div className="pure-control-group">
-                          <label htmlFor="senha">Senha</label> 
-                          <input id="senha" type="password" name="senha"  value={this.state.senha} onChange={this.setSenha}/>                                      
-                        </div>
+                      <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+                        <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome"/>                                              
+                        <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email"/>                                              
+                        <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha"/>                                                                      
                         <div className="pure-control-group">                                  
                           <label></label> 
                           <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
@@ -113,7 +104,7 @@ class App extends Component {
                         </thead>
                         <tbody>
                           {
-                            this.state.lista.map(function(autor) {
+                            this.state.lista.map(function(autor){
                               return (
                                 <tr key={autor.id}>
                                   <td>{autor.nome}</td>
@@ -126,7 +117,9 @@ class App extends Component {
                       </table> 
                     </div>             
                   </div>
-                </div>
+                </div>            
+
+
       </div>     
     );
   }
